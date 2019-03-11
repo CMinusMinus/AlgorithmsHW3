413dde20bee8b394ed1d9420b3bea3bfaa626d8f
Brandon Wolff <wolffba@mail.uc.edu>
2019-03-11 15:20:58 -0400

    Implemented Adjacency matrix

--- main.cpp
+++ main.cpp
@@ -8,30 +8,35 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
-  std::vector<int>* vertices = new std::vector<int>();
-  set<int*>* edges = new set<int*>();
-
   int numVerts = atoi(argv[1]);
   printf("Number of Vertices: %i\n", numVerts);
-  for (int i = 0; i < numVerts; i++) {
-    vertices->push_back(i);
+  vector<vector<int> > matrix(numVerts, vector<int>(numVerts));
+
+  for (int k = 0; k < (argc - 2)/2; k++) {
+    int first = atoi(argv[k * 2 + 2]);
+    int second = atoi(argv[k * 2 + 3]);
+    printf("Edge created: %i, %i\nNumber of edges: %i\n", first, second, k+1);
+    // cout << "Edge created: [" << argv[k*2 + 1] >> ", " << argv[(k+1) * 2] << "]" << endl;
+    matrix.at(first).at(second) = 1;
+    matrix.at(second).at(first) = 1;
   }
 
-  for (int j = 0; j < (argc - 2)/2; j++) {
-    int* edgeToAdd = new int;
-    int first = atoi(argv[j * 2 + 2]);
-    int second = atoi(argv[j * 2 + 3]);
-    if (first < second) {
-      edgeToAdd[0] = atoi(argv[j * 2 + 2]);
-      edgeToAdd[1] =  atoi(argv[j * 2 + 3]);
-    } else {
-       edgeToAdd[0] =  atoi(argv[j * 2 + 3]);
-      edgeToAdd[1] = atoi(argv[j * 2 + 2]);
+  cout << "Adjacency matrix:" << endl;
+  for(int i = 0; i < numVerts; i++)
+  {
+    for(int j = 0; j < numVerts; j++)
+    {
+      printf("%i ", matrix.at(i).at(j));
     }
-    edges->insert(edgeToAdd);
-    printf("Edge created: %i, %i\nNumber of edges: %i\n", edgeToAdd[0], edgeToAdd[1], edges->size());
-    // cout << "Edge created: [" << argv[j*2 + 1] >> ", " << argv[(j+1) * 2] << "]" << endl;
+    printf("\n");
   }
 
   return 0;
+}
+
+int Diameter(vector<vector<int> > matrix) {
+  // If the graph is connected, 
+  // use BFS to find and return the length of
+  // the longest shortest path between two nodes 
+  // Else, return -1
 }
