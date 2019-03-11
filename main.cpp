c31e234772784548c9f247bb3f042704b6a690ec
Brandon Wolff <wolffba@mail.uc.edu>
2019-03-11 16:29:23 -0400

    Implemented diameter function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h> 
 #include <vector>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,8 +23,7 @@ int main(int argc, char ** argv) {
   }
 
   cout << "Adjacency matrix:" << endl;
-  for(int i = 0; i < numVerts; i++)
-  {
+  for(int i = 0; i < numVerts; i++) {
     for(int j = 0; j < numVerts; j++)
     {
       printf("%i ", matrix.at(i).at(j));
@@ -34,9 +34,33 @@ int main(int argc, char ** argv) {
   return 0;
 }
 
+vector<int> BFS(vector<vector<int> > matrix, int s) {
+  //
+}
+
 int Diameter(vector<vector<int> > matrix) {
   // If the graph is connected, 
   // use BFS to find and return the length of
   // the longest shortest path between two nodes 
   // Else, return -1
+  int diameter = 0;
+  for(int i = 0; i < matrix.size(); i++) {
+    vector<int> visited = BFS(matrix, i);
+    int disconnected = count(visited.begin(), visited.end(), 0);
+    int max = *max_element(visited.begin(), visited.end());
+    if (disconnected > 1) {
+      diameter = -1; 
+      break;
+    } else if (max > diameter) {
+      diameter = max;
+    }
+  }
+  
+  return diameter;
+}
+
+void Components(vector<vector<int> > matrix) {
+  // If the graph was disconnected,
+  // use BFS from 0-n to find all separate parts of the graph,
+  // and print out the list of vertices in each part.
 }
